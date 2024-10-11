b0ca95c5983335a135b6d4bde0ccd0de13786a82
AntonSolianyk <s30252@pjwstk.edu.pl>
2024-10-11 14:49:15 +0200

    added substractor

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Adder adder =new Adder();
         System.out.println(adder.add(1,2));
-        Subtructor subtructor = new Subtructor();
+        Substractor subtructor = new Substractor();
         System.out.println(subtructor.subtract(6,3));
     }
 }
--- src/Substractor.java
+++ src/Substractor.java
@@ -1,2 +1,10 @@
 public class Substractor {
+//    int a, b;
+//    public Substractor(int a, int b){
+//        this.a = a;
+//        this.b = b;
+//    }
+    public int subtract(int a, int b){
+        return a-b;
+    }
 }
