414007ea26ff40150a3aa380bbb1a13886185cd3
AntonSolianyk <s30252@pjwstk.edu.pl>
2024-10-11 14:43:49 +0200

    added substractor

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,9 @@
 /// OK,I will add 'Adder' amd s30252 will add 'Subtractor'
 public class Main {
     public static void main(String[] args) {
-    Adder adder =new Adder();
-
+        Adder adder =new Adder();
         System.out.println(adder.add(1,2));
-        Subtructor subtructor = new Subtructor;
+        Subtructor subtructor = new Subtructor();
         System.out.println(subtructor.subtract(6,3));
-
     }
 }
