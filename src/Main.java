d82fb88cf8bad30b5a2298523e4ed3477f6a1bfc
s30062 <s30062@pjwstk.edu.pl>
2024-10-11 14:19:36 +0200

    Initial project setup

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+    Adder adder =new Adder();
+
+        System.out.println(adder.add(1,2));
+        Subtructor subtructor = new Subtructor;
+        System.out.println(subtructor.subtract(6,3));
+
+    }
+}
